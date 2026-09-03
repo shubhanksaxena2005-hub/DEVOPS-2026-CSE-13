0daa9123afa837f09445ae54d0e890618d3dd142
surbhi-2007 <chaudharysurbhi731@gmail.com>
2026-09-03 13:31:59 +0530

    Add backend configuration file

---
+++ AI_Agriculture_Assistant_Backend_Complete/config/cloudinary.js
@@ -0,0 +1,2 @@
+// cloudinary.js
+export default { message:'cloudinary.js placeholder for Phase 2 backend'};
---
+++ AI_Agriculture_Assistant_Backend_Complete/config/gemini.js
@@ -0,0 +1,2 @@
+// gemini.js
+export default { message:'gemini.js placeholder for Phase 2 backend'};
